1c44cad767af14d1eda6358f38accda648479e75
Josh Hepworth <zeplar1@gmail.com>
2024-01-17 14:10:02 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Granoia/mitchell-121.git
+
+go 1.21.3
---
+++ src/homework/hw2/hw2.go
@@ -0,0 +1,22 @@
+package hw2
+
+// IsPrime returns true if the argument is prime.
+func IsPrime() {
+
+}
+
+// SumCubes returns the sum of the cubes of the first n numbers.
+func SumCubes() {
+}
+
+// Fibonacci returns the nth Fibonacci3 number.
+func Fibonacci3() {
+}
+
+// Polymax returns the maximum of a polynomial function -x^4 + 3x^2 -y^4 + 5y^2.
+func Polymax() {
+}
+
+// Collatz returns the number of steps it takes to reach 1 under Collatz's conjecture.
+func Collatz() {
+}
