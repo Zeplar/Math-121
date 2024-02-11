4a5023cbe6cc03b019a35432d99187554a0d00be
Mitchell <mitchell.k.kim@gmail.com>
2024-02-11 16:28:24 -0600

    submitting homework 3

---
+++ src/homework/hw3/hw3.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+type poly1 func(int) int
+type poly2 func(int) bool
+
+func max_of_two(fn1 poly1, fn2 poly1, x int) int {
+	if fn1(x) > fn2(x) {
+		return fn1(x)
+	} else {
+		return fn2(x)
+	}
+}
+
+func max_of_funcs(fn1 poly1, fn2 poly1) poly1 {
+	return func(x int) int {
+		if fn1(x) > fn2(x) {
+			return fn1(x)
+		} else {
+			return fn2(x)
+		}
+	}
+}
+
+func evenX(x int) bool {
+	if x%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+var is_even = func(x int) bool {
+	return evenX(x)
+}
+
+func conditional_printchan(test poly2) poly1 {
+	return func(x int) int {
+		return x
+	}
+}
+
+var print_evenchan = conditional_printchan(evenX)
+
+func prints_evenchan() {
+	conditional_printchan(evenX)
+	f := conditional_printchan(evenX)
+	fmt.Println(f)
+}
+
+func main() {
+
+}
+
+//Excercise 6:Im not sure why it would take one function "g" over the other,
+//				just know that when I run it, it takes the first one, "x+3" and spits out a = 5 ; b = 6
